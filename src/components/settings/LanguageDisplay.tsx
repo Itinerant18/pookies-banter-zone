a6b8646b41b7cff381ff45e75412c90698f8aae5
Aniket karmakar <94900936+Itinerant18@users.noreply.github.com>
2025-03-07 17:58:55 +0530

    Update LanguageDisplay.tsx

--- src/components/settings/LanguageDisplay.tsx
+++ src/components/settings/LanguageDisplay.tsx
@@ -1,16 +1,15 @@
-
-import React from 'react';
+import React from "react";
 
 const LanguageDisplay = () => {
   return (
-    <div className="flex items-center justify-between">
+    <div className="flex items-center justify-center">
       <div className="flex items-center space-x-2">
         <div className="h-5 w-5 flex items-center justify-center text-muted-foreground">
-        <img
-                        src="/translate.png"
-                        alt="Profile"
-                        className="w-5 h-5 object-contain"
-                      />
+          <img
+            src="/translate.png"
+            alt="Profile"
+            className="w-5 h-5 object-contain"
+          />
         </div>
         <div>
           <p className="text-sm font-medium">Language</p>
